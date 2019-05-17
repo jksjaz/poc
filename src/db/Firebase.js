bbcb6652623ab96d295b20ccf2c4a875f83dfb0a
Jaskaran Singh <44025944+jksjaz@users.noreply.github.com>
2019-05-17 18:53:44 -0500

    okok

--- src/db/Firebase.js
+++ src/db/Firebase.js
@@ -1,13 +1,7 @@
 import * as firebase from "firebase"
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAbhlR_-Ll89r-BPpyshB3fbF34yIjw-Zw",
-    authDomain: "ecommerce-f48be.firebaseapp.com",
-    databaseURL: "https://ecommerce-f48be.firebaseio.com",
-    projectId: "ecommerce-f48be",
-    storageBucket: "ecommerce-f48be.appspot.com",
-    messagingSenderId: "496785036623",
-    appId: "1:496785036623:web:48ba9888489408a1"
+    // Configuration Goes here
 };
 
 firebase.initializeApp(firebaseConfig)
